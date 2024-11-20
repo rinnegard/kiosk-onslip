dfb9765b3b6fb54bdbb7c0f0113d719ba0ee9f51
rinnegard <57401904+rinnegard@users.noreply.github.com>
2024-11-20 14:00:23 +0100

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'kiosk-onslip',
+  webDir: 'dist'
+};
+
+export default config;
