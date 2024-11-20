48c6bc86b866a6c50c7003cb3fa62fca00807c03
rinnegard <57401904+rinnegard@users.noreply.github.com>
2024-11-20 16:38:40 +0100

    adding onslip sdk and testing getting products

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,48 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+} from "@ionic/react";
+
+import "./Tab1.css";
+import { useEffect, useState } from "react";
+
+import { API, webRequestHandler } from "@onslip/onslip-360-web-api";
+
+API.initialize(webRequestHandler({}));
+
+const hawk = import.meta.env.VITE_HAWK_ID;
+const key = import.meta.env.VITE_KEY;
+const realm = import.meta.env.VITE_REALM;
+
+const api = new API("https://test.onslip360.com/v1/", realm, hawk, key);
 
 const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
-  );
+    const [data, setData] = useState<any>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await api.listProducts();
+            setData(response);
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Products</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <pre>{JSON.stringify(data, null, 2)}</pre>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab1;
