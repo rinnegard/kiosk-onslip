58f34bf55b8addc239e0543c1dc9de36bcc066b8
rinnegard <57401904+rinnegard@users.noreply.github.com>
2024-11-26 12:51:59 +0100

    fixed cart badge counter

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -5,19 +5,17 @@ import { useCart } from "../contexts/cartContext";
 export default function CartIcon() {
     const { state } = useCart();
 
+    const cartSize = state.items.reduce((sum, item) => {
+        return sum + item.quantity;
+    }, 0);
+
     return (
         <IonRouterLink className="cart-icon-container" href="/cart">
             <div className="cart-icon-wrapper">
-                <IonIcon 
-                    icon={cart} 
-                    className="cart-icon"
-                />
+                <IonIcon icon={cart} className="cart-icon" />
                 {state.items.length > 0 && (
-                    <IonBadge 
-                        color="danger" 
-                        className="cart-badge"
-                    >
-                        {state.items.length}
+                    <IonBadge color="danger" className="cart-badge">
+                        {cartSize}
                     </IonBadge>
                 )}
             </div>
